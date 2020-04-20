4029dec4ac9f960755206c4a88f393ff51c8ee46
dainakao <dainakao@gmail.com>
2020-04-21 00:04:59 +0900

    部分検索も可能に

--- NGRAM_SEARCH/src/Ngram_Search/Search_Index.java
+++ NGRAM_SEARCH/src/Ngram_Search/Search_Index.java
@@ -70,38 +70,65 @@ public class Search_Index {
 		}
 
 		File file = new File(pass_name + "\\Ngram");
-		File files[] = file.listFiles();//ファイル名リスト
+		File[] files = file.listFiles();//ファイル名リスト
 		boolean[][] answers = new boolean[files.length][search_words.length];
 
 		//検索部位
 		System.out.println("～検索結果～");
+		//まずは完全一致検索から
 		System.out.println("・完全一致");
 		boolean ans = false;//一時的な変数
 		for(int i=0; i<index.size(); i=i+3) {
 			if(index.get(i).equals(search_words[0])) {
 				ans=true;
 				String str = this.Read_String(index.get((i)+1).substring(6,index.get((i)+1).length()), pass_name+"data\\");//引っかかったファイルをStringに
-					if(Objects.equals(str.substring(Integer.parseInt(index.get((i)+2)),Integer.parseInt(index.get((i)+2))+search_word.length()), search_word)) {
-						System.out.println(index.get((i)+1)+","+index.get((i)+2));
+				if(Objects.equals(str.substring(Integer.parseInt(index.get((i)+2)),Integer.parseInt(index.get((i)+2))+search_word.length()), search_word)) {
+					System.out.println(index.get((i)+1)+","+index.get((i)+2));
+					System.out.println("→" + str.substring(Integer.parseInt(index.get((i)+2))-10,Integer.parseInt(index.get((i)+2))+search_word.length()+10));
+				}
+				/*
+				//部分検索の第一段階(先頭の2文字)
+				for(int j=0; j<files.length; j++) {
+					StringBuilder stringBuilder = new StringBuilder();
+					stringBuilder.append(pass_name);
+					stringBuilder.append("Ngram\\");
+					stringBuilder.append(index.get((i)+1));
+					String c = files[j].toString();
+					if(c.equals(stringBuilder.toString())) {
+						System.out.println("一致");
+						answers[j][0] = true;
 					}
+				}
+				*/
 			}
 		}
 		if(ans == false)System.out.println("該当無し");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		//本格的に部分検索
+		for(int h=0; h<search_words.length; h++) {
+			for(int i=0; i<index.size(); i=i+3) {
+				if(index.get(i).equals(search_words[h])) {
+					for(int j=0; j<files.length; j++) {
+						StringBuilder stringBuilder = new StringBuilder();
+						stringBuilder.append(pass_name);
+						stringBuilder.append("Ngram\\");
+						stringBuilder.append(index.get((i)+1));
+						String c = files[j].toString();
+						if(c.equals(stringBuilder.toString())) {
+							System.out.println("一致");
+							answers[j][h] = true;
+						}
+					}
+				}
+			}
+		}
+		for(int h=0; h<answers.length; h++) {
+			System.out.print(files[h]+";");
+			for(int i=0; i<answers[h].length; i++) {
+				System.out.print(answers[h][i]+",");
+			}
+			System.out.println();
+		}
 	}
 
 
