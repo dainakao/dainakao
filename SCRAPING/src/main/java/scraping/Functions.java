169108f9edb482be192f91c87c055ebcf2771643
dainakao <dainakao@gmail.com>
2020-06-22 02:16:38 +0900

    修正

--- SCRAPING/src/main/java/scraping/Functions.java
+++ SCRAPING/src/main/java/scraping/Functions.java
@@ -9,8 +9,8 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class Functions {
-	//本文を抜き出す
-	public static String body_scr(String str) {
+	//全文を抜き出す
+	public static String body_scr1(String str) {
 		String body=null;
 		//try-catch文が必要
 		try {
@@ -31,20 +31,61 @@ public class Functions {
 		return body;
 	}
 
+	//本文の指定箇所を抜き出す
+	public static String body_scr2(String str) {
+		String ans = "";
+		String body=null;
+		//try-catch文が必要
+		try {
+			//Document A = Jsoup.connect("url").get(); urlにスクレイピング対象
+			Document doc = Jsoup.connect(str).get();
+
+			//Elements B = A.select("タグ"); この形でソースに含まれるタグで指定された範囲を書き出す
+			//DIVタグを書き出す
+			//Elements elm_p = doc.select("DIV");
+			//クラス名を指定して書き出す
+			Elements elm = doc.getElementsByClass("mw-parser-output");
+			Elements elm_p = elm.select("p");
+
+			//拡張for文
+			for(Element elms : elm_p) {
+				body = elms.text();
+				//System.out.println(body);
+				ans += body;
+			}
+
+			//例外処理
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		return ans;
+	}
+
 	//タイトルとリンクを２次配列で返却
 	public static String[][] list_scr(String str) {
 		ArrayList<String> title = new ArrayList<String>();
 		ArrayList<String> url = new ArrayList<String>();
-		//status 0=書き出さない・1=書き出す
+		//status 0=書き出さない・1=1Pか2Pの時の書き出し・2=2P3Pの判断・3=3Pの時の書き出し・4=書き出す
 		int status = 0;
 		Elements links = url_get(str);
 		for (Element link : links) {
 			if(status == 1) {
+				if(link.text().contentEquals("前のページ")||link.text().contentEquals("次のページ"))break;
+				title.add(link.text());
+				url.add(link.attr("href"));
+			}if(status == 2) {
+				if(link.text().contentEquals("次のページ")) {
+					status = 1;
+				}else {
+					status = 3;
+				}
+			}if(status == 3) {
 				if(link.text().contentEquals("前のページ"))break;
 				title.add(link.text());
 				url.add(link.attr("href"));
-			}else {
+			}if(status == 0){
 				if(link.text().contentEquals("次のページ")) status = 1;
+				if(link.text().contentEquals("前のページ")) status = 2;
 			}
 		}
 		String[][] Strings = new String[title.size()][2];
